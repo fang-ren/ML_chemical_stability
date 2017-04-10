2e830047fb04380c3045dc8640b56ab7c64a132f
fang-ren <ren.fang.ren@gmail.com>
2017-04-10 10:42:14 -0700

    update directory

--- import_test_data_new.py
+++ import_test_data_new.py
@@ -9,7 +9,7 @@ Created on Wed 12/14/16
 import pandas as pd
 import numpy as np
 
-def import_test_data(test_file = 'challenge_data\\challenge_data\\test_data.csv'):
+def import_test_data(test_file = 'data\\data\\test_data.csv'):
     # import data into dataframe
     data = pd.read_csv(test_file)
 
@@ -19,6 +19,4 @@ def import_test_data(test_file = 'challenge_data\\challenge_data\\test_data.csv'
     # delete the first two columns, which can be represented exclusively by "formulaA_elements_Number" and "formulaB_elements_Number"
     del data[names[0]]
     del data[names[1]]
-    names = list(data) # update column names
-    names = np.array(names)
-    return names, data
+    return data
--- import_training_data_new.py
+++ import_training_data_new.py
@@ -9,7 +9,7 @@ Created on Wed 12/14/16
 import pandas as pd
 import numpy as np
 
-def import_training_data(training_file = 'challenge_data\\challenge_data\\training_data.csv'):
+def import_training_data(training_file = 'data\\data\\training_data.csv'):
     # import data into dataframe
     data = pd.read_csv(training_file)
 
